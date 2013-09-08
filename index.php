4bb39635fd64f6ca757ae6399413391c0f6080c6
Yihang Ho <hoyihang5@gmail.com>
2013-09-08 10:50:18 +0800

    Completely revamped the tutorial.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>LaTeX Editor</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/tutorial.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="js/tutorial.js"></script>
 	<script src="js/bootstrap.min.js"></script>
@@ -16,13 +17,37 @@
 	<script src="js/mathjax-livepreview.js"></script>
 	<script>
 	$(document).ready(function(){
-		var previewer = Initialize($("textarea#input"), $("div#ScribblePreview"), $("div#ScribbleBuffer"), $(".insert-latex"));
+		var previewer = Initialize($("textarea#ScribbleInput"), $("div#ScribblePreview"), $("div#ScribbleBuffer"), $(".insert-latex"));
 	});
 	</script>
 </head>
 <body>
 	<div class="container">
-		<div class="navbar navbar-default">
+		<div class="alert alert-warning alert-dismissable" style="display:none;" id="tutorial-alert">
+			<button type="button" class="close" aria-hidden="true" onclick="$('div#tutorial-alert').fadeOut();">&times;</button>
+			<strong>Hmm...</strong> <span id="alert-message"></span>
+		</div>
+		
+		<div id="tutorial-container">
+			<button id="tutorial-start" class="btn btn-default">Start tutorial</button>
+			<div class="panel panel-default" id="tutorial" style="display:none;">
+				<div class="panel-body" id="instruction">
+			    	Welcome to the \(\LaTeX\) tutorial brought to you by Scribble! Before we get started, it is good that we get familiar with the user interface here. Click Next to continue.
+				</div>
+				<div class="panel-footer" style="overflow:hidden;">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default tutorial-change-step" id="prev" disabled data-force="true">Prev</button>
+						<button type="button" class="btn btn-default" id="reset" disabled>Reset</button>
+						<button type="button" class="btn btn-default tutorial-change-step" id="next" data-goto=2>Next</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="well well-lg" id="ScribblePreview" style="display:none;"></div>
+		<div class="well well-lg" id="ScribbleBuffer" style="display:none;"></div>
+
+		<div class="navbar navbar-default" id="latex-selector">
 			<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 				<span class="sr-only">Toggle navigation</span>
@@ -38,17 +63,10 @@
 			</div>
 		</div>
 
-		<div id="tutorial">
-			<button id="tutorial-start" class="btn btn-default">Start tutorial</button>
-			<p id="instruction" style="display:none;">\({}\)</p>
-			<button id="tutorial-next" class="btn btn-default" style="display:none;">Next</button>
-		</div>
-
 		<div>
-			<textarea id="input" rows=15 style="font-family: 'Courier New', Courier, monospace; width:100%;"></textarea>
+			<textarea id="ScribbleInput" rows=15 style="font-family: 'Courier New', Courier, monospace; width:100%;"></textarea>
 		</div>
-		<div id="ScribblePreview"></div>
-		<div id="ScribbleBuffer"></div>
+		
 	</div>
 </body>
 </html>
--- js/tutorial.js
+++ js/tutorial.js
@@ -1,75 +1,175 @@
-$(document).ready(function(){
-	$("button#tutorial-start").click(function(){
-		$("button#tutorial-start").hide();
-		$("button#tutorial-next").show();
-		$("button#tutorial-next").data("current-step", 1);
-		$("p#instruction").show();
-		$("p#instruction").text("Hi! Thanks for reading this tutorial. This tutorial aims to teach the basic of \\(\\LaTeX\\). \\(\\LaTeX\\) is a markup language that allows users to easily input math equations into computer.");
-		MathJax.Hub.Queue(["Typeset",MathJax.Hub,"instruction"]);
-	});
+var prev_element = false;
+var animate = function(element) {
+	if (prev_element && prev_element.hasClass('animated tada')) prev_element.removeClass('animated tada');
+	element.addClass('animated tada');
+	prev_element = element;
+}
+var kill_animate = function() {
+	if (prev_element) prev_element.removeClass('animated tada');
+	prev_element = false;
+}
+var update_step = function(cur_step) {
+	if (cur_step != 1) {
+		$("button#prev").data("goto", cur_step-1);
+		$("button#prev").removeAttr("disabled");
+	} else {
+		$("button#prev").attr("disabled", "disabled");
+	}
+	
+	$("button#next").data("goto", cur_step+1);
 
-	$("button#tutorial-next").click(function(){
-		if ($(this).data("current-step") == 1)
-		{
-			$(this).data("current-step", 2);
-			$("p#instruction").text("In general, there are two modes that are available in \\(\\LaTeX\\): inline mode and displayed mode. Let us first see what inline mode is. First, in the sentence present below, move the cursor to right after the colon(:) and click on the \"Inline mode\" button");
-			MathJax.Hub.Queue(["Typeset",MathJax.Hub,"instruction"]);
-			$("textarea#input").val("Let's see what inline mode is. Look here: Amazing!");
+	if (cur_step < 10) $("button#next").removeAttr("disabled");
+	else $("button#next").attr("disabled", "disabled");
+}
+var hide_all = function() {
+	$("div#tutorial-alert").hide();
+	$("textarea#ScribbleInput").hide();
+	$("div#ScribblePreview").hide();
+	$("div#ScribbleBuffer").hide();
+	$("div#latex-selector").hide();
+	$("button#reset").attr("disabled", "disabled");
+}
+
+var goto = function(step, force) {
+	if (!step) return;
+	if (step < 1 || step > 10) {
+		return;
+	}
+	
+	if (step == 1) {
+		hide_all();
+		kill_animate();
+		$("div#tutorial div.panel-body").html("Welcome to the \\(\\LaTeX\\) tutorial brought to you by Scribble! Before we get started, it is good that we get familiar with the user interface here. Click Next to continue.");
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub,"instruction"]);
+	}
+	if (step == 2) {
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		animate($("textarea#ScribbleInput"));
+		$("div#tutorial div.panel-body").html("What is shown to you right now is the <strong>textarea</strong>. It is there for you to write stuff down.");
+	}
+	if (step == 3) {
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Hello World!");$("textarea#ScribbleInput").keyup();}, 200);
+		$("div#tutorial div.panel-body").html("The grey box that appears above the textarea is the <strong>live preview box</strong>. Everything that you have written in the textarea will be shown there. Try it out!");
+	}
+	if (step == 4) {
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Hello World!");$("textarea#ScribbleInput").keyup();}, 200);
+		$("div#tutorial div.panel-body").html("The grey box that is sandwiched between the live preview box and textarea is the \\(\\LaTeX\\) <strong>selector</strong>. Basically, this selector allows you to use \\(\\LaTeX\\) without knowing its markup language.");
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub,"instruction"]);
+	}
+	if (step == 5) {
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Let's try out the inline mode: Lorem ipsum dolor sit amet, consectetur adipiscing elit.");$("textarea#ScribbleInput").keyup();$("button#reset").data("default-text", $("textarea#ScribbleInput").val());}, 200);
+		$("button#reset").removeAttr("disabled");
+		$("div#tutorial div.panel-body").html("In \\(\\LaTeX\\), there are two modes to enter math: inline mode and displayed mode. We shall see what inline mode is first. In the textarea below, move the cursor to right after the colon, and choose 'Inline mode' from the selector.<br>When you are ready, click Next. If you think you screwed something up, just click Reset.");
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub,"instruction"]);
+	}
+	if (step == 6) {
+		if ($("textarea#ScribbleInput").val() != "Let's try out the inline mode:\\( \\) Lorem ipsum dolor sit amet, consectetur adipiscing elit." && !force) {
+			$("span#alert-message").html("Seems like you are not doing it right. Why not give it a try again?");
+			$("div#tutorial-alert").fadeIn();
+			return;
 		}
-		else if ($(this).data("current-step") == 2)
-		{
-			if ($("textarea#input").val() == "Let's see what inline mode is. Look here:\\( \\) Amazing!")
-			{
-				$(this).data("current-step", 3); 
-				$("p#instruction").text("That's right! You should see that '\\( \\)' being inserted into the text. All math symbol should be put in between them. Now, make sure that the cursor is right before the second slash, and click \"Common\" then \"Square root\"");
-			}
-			else
-			{
-				alert("nope...");
-			}
+		hide_all();
+		kill_animate();
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Let's try out the inline mode:\\( \\) Lorem ipsum dolor sit amet, consectetur adipiscing elit.");$("textarea#ScribbleInput").keyup();$("button#reset").data("default-text", $("textarea#ScribbleInput").val());}, 200);
+		$("button#reset").removeAttr("disabled");
+		$("div#tutorial div.panel-body").html("You should be able to see that '\\( \\)' appears right after the colon. This is the delimiter for inline mode - we will be entering all the math stuff in between this pair. Now, move the cursor to right before the second slash, click 'Common' in the selector and choose 'Square root'.<br>Click next when you are ready and, again, if you think you're stuck, click Reset.");
+	}
+	if (step == 7) {
+		if ($("textarea#ScribbleInput").val() != "Let's try out the inline mode:\\( \\sqrt{b}\\) Lorem ipsum dolor sit amet, consectetur adipiscing elit." && !force) {
+			$("span#alert-message").html("Seems like you are not doing it right. Why not give it a try again?");
+			$("div#tutorial-alert").fadeIn();
+			return;
 		}
-		else if ($(this).data("current-step") == 3)
-		{
-			if ($("textarea#input").val() == "Let's see what inline mode is. Look here:\\( \\sqrt{b}\\) Amazing!")
-			{
-				$(this).data("current-step", 4);
-				$("p#instruction").text("Take a look at the preview under the textarea. You can see that the square root sign appear among the other text. We say that the square root sign appears inline with other text and hence, it is called 'inline mode'. Before we proceed to explore what Displayed mode is, you should play around with other math formulae. Just make sure that all formulae appears in between \\( and \\). (Try not putting them in between \\( and \\) and see what happens) When you are ready, click next to continue.");
-			}
-			else
-			{
-				alert("nope...");
-			}
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Let's try out the inline mode:\\( \\sqrt{b}\\) Lorem ipsum dolor sit amet, consectetur adipiscing elit.");$("textarea#ScribbleInput").keyup();}, 200);
+		$("div#tutorial div.panel-body").html("That's right. You should now be able to see a beautifully rendered square root sign in the live preview box. From what we can see now, the math symbol appears in between other text outside the delimiter - it can be said that the math symbol is shown <em>inline</em> with other text, hence, it is called inline mode. Before we move on, please feel free to experiment with other symbols. Also, try to change the \\(b\\) in the square root to something else.<br>When you are ready, click Next.");
+	}
+	if (step == 8) {
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Let's try out the displayed mode this time: Curabitur sodales, lectus eget auctor egestas, diam odio egestas sem, a tincidunt nibh tellus ut risus.");$("textarea#ScribbleInput").keyup();$("button#reset").data("default-text", $("textarea#ScribbleInput").val());}, 200);
+		$("button#reset").removeAttr("disabled");
+		$("div#tutorial div.panel-body").html("This time, we are going to see what displayed mode is. Just like last time, move the cursor in the textarea below to right after the colon. This time, choose Displayed mode from the selector.");
+	}
+	if (step == 9) {
+		if ($("textarea#ScribbleInput").val() != "Let's try out the displayed mode this time:\\[ \\] Curabitur sodales, lectus eget auctor egestas, diam odio egestas sem, a tincidunt nibh tellus ut risus." && !force) {
+			$("span#alert-message").html("Seems like you are not doing it right. Why not give it a try again?");
+			$("div#tutorial-alert").fadeIn();
+			return;
 		}
-		else if ($(this).data("current-step") == 4)
-		{
-			$(this).data("current-step", 5);
-			$("p#instruction").text("Now, we are ready to see what Displayed mode is. Again, move your cursor to right after the colon. This time, click \"Displayed mode\"");
-			$("textarea#input").val("Let's see what displayed mode is. Look here: Amazing!");
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Let's try out the displayed mode this time:\\[ \\] Curabitur sodales, lectus eget auctor egestas, diam odio egestas sem, a tincidunt nibh tellus ut risus.");$("textarea#ScribbleInput").keyup();$("button#reset").data("default-text", $("textarea#ScribbleInput").val());}, 200);
+		$("button#reset").removeAttr("disabled");
+		$("div#tutorial div.panel-body").html("Great work! Now, move the cursor to right before the second slash. Click Series from the selector and then choose Sigma notation.<br>When you're done, click Next. If you messed things up, click Reset.");
+	}
+	if (step == 10) {
+		if ($("textarea#ScribbleInput").val() != "Let's try out the displayed mode this time:\\[ \\sum^{n}_{i=0}a_i\\] Curabitur sodales, lectus eget auctor egestas, diam odio egestas sem, a tincidunt nibh tellus ut risus." && !force) {
+			$("span#alert-message").html("Seems like you are not doing it right. Why not give it a try again?");
+			$("div#tutorial-alert").fadeIn();
+			return;
 		}
-		else if ($(this).data("current-step") == 5)
-		{
-			if ($("textarea#input").val() == "Let's see what displayed mode is. Look here:\\[ \\] Amazing!")
-			{
-				$(this).data("current-step", 6);
-				$("p#instruction").html("Make sure that the cursor is now right before the second slash. Copy and paste the following into the textarea:<br><code>T = 2 \\pi \\sqrt{ \\frac{l}{g} }</code>");
-			}
-			else
-			{
-				alert("nope...");
-			}
-		}
-		else if ($(this).data("current-step") == 6)
-		{
-			if ($("textarea#input").val() == "Let's see what displayed mode is. Look here:\\[ T = 2 \\pi \\sqrt{ \\frac{l}{g} }\\] Amazing!")
-			{
-				// $(this).data("current-step", 7);
-				$("p#instruction").html("Right now, you should be able to see the equation for the period of a simple pendulum displayed in a new line and centered. This is how you math equation will be shown in displayed mode.<br>In escence, when writing in LaTeX, you can work in two modes: inline and displayed. For inline mode, math formulae will be displayed inline with other text; in displayed mode, however, math formulae will be shown in it own line. Also, you should take note that all math formulae should appear inside \\( \\) or \\[ \\]. It is possible to have multiple \\( \\) or \\[ \\] in a single document.<br>Now, experiment with other formulae in these two modes and have fun!");
-				$(this).hide();
-			}
-			else
-			{
-				alert("nope...");
-			}
-		}
+		hide_all();
+		kill_animate();
+		$("textarea#ScribbleInput").show();
+		$("div#latex-selector").show();
+		$("textarea#ScribbleInput").val("");
+		$("textarea#ScribbleInput").keyup();
+		setTimeout(function(){$("textarea#ScribbleInput").val("Let's try out the displayed mode this time:\\[ \\sum^{n}_{i=0}a_i\\] Curabitur sodales, lectus eget auctor egestas, diam odio egestas sem, a tincidunt nibh tellus ut risus.");$("textarea#ScribbleInput").keyup();$("button#reset").data("default-text", $("textarea#ScribbleInput").val());}, 200);
+		$("div#tutorial div.panel-body").html("Nice. As you can see, although the syntax for sigma notation appears in between other text, it is shown on its own line and is centered. This is how displayed mode shows math formula. Also take note that the delimiter for displayed mode is \\[ \\] while the delimiter for inline mode is \\( \\).<br>You have now reached the end of the tutorial. The operating model of Scribble is almost the same as what you have seen here, except you can specify a (optional) title and have to save when you're ready. Feel free to experiment with other symbols using inline mode or displayed mode before you go. Have fun!");
+	}
+	update_step(step);
+}
+
+$(document).ready(function(){
+	$("button.tutorial-change-step").click(function(){
+		goto($(this).data("goto"), $(this).data("force"));
+	});
+	$("button#reset").click(function(){
+		$("textarea#ScribbleInput").val($(this).data("default-text"));
+		$("textarea#ScribbleInput").keyup();
+	});
+	$("button#tutorial-start").click(function(){
+		$(this).hide();
+		$("div#tutorial").show();
+		goto(1);
 	});
 });
